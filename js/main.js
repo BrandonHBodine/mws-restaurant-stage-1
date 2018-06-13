200060245fdf4bf1b46cafd03df7aa0ec7afbdf3
Brandon H Bodine <brandonhbodine@gmail.com>
2018-06-12 19:57:56 -0600

    refactor: refactor and small update
    The final commit before subtmitting this project for stage 1

--- js/main.js
+++ js/main.js
@@ -194,21 +194,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 	});
 };
 
-/***
- * Register Service worker if avliable in browser
- **/
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('./sw.js').then(
-			function(registration) {
-				console.log(
-					'Service Worker file registration successful!',
-					registration
-				);
-			},
-			function(err) {
-				console.log('Service Worker failed: ', err);
-			}
-		);
-	});
-}
